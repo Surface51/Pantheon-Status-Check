652f503c5be334c20bbbb05f582d3dca5b98ae89
John Runyard <jrunyard3ft@gmail.com>
2023-09-14 14:41:17 -0500

    Changed terminus command to a more readable alias.

--- status.sh
+++ status.sh
@@ -5,7 +5,7 @@
 # Example usage: bash status.sh | tee -a results.txt
 
 # Reading names of all sites from terminus
-readarray sitelist < <(terminus site:list --field=name --format=list)
+readarray sitelist < <(terminus sites --field=name --format=list)
 
 for i in "${sitelist[@]}"; do
     # trim newlines from output
